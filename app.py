c8d4f6fc57cc1166636689865da9c9645b0cf0e2
Asri Pramudiya <61897225+Asripramudiya@users.noreply.github.com>
2026-07-12 20:58:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import joblib
 import numpy as np
 from flask import Flask, render_template, request
 from preprocessing import preprocess_text
+import os
 
 BASE_DIR = Path(__file__).resolve().parent
 app = Flask(__name__)
@@ -63,4 +64,10 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
